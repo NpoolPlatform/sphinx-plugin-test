abb55a796de0b79922c41af0e1841ffeede6274b
chen <531759321@qq.com>
2022-03-23 04:24:01 +0000

    usdt-erc20

--- install-teth.sh
+++ install-teth.sh
@@ -52,6 +52,7 @@ function install_sphinx_plugin() {
   rm -rf /usr/local/bin/sphinx-plugin
   cp output/linux/amd64/sphinx-plugin /opt/sphinx-plugin/
   cp output/linux/amd64/sphinx-plugin /usr/local/bin/
+  sed -i 's/ENV_COIN_API=/ENV_COIN_API=127.0.0.1:8545/g' systemd/sphinx-plugin.service
   sed -i '/ENV_COIN_API=/a\Environment="ENV_COIN_NET=test"' systemd/sphinx-plugin.service
   sed -i '/ENV_COIN_API=/a\Environment="ENV_COIN_TYPE=ethereum,usdt-erc20"' systemd/sphinx-plugin.service
   cp cmd/sphinx-plugin/SphinxPlugin.viper.yaml /etc/SphinxPlugin/
@@ -60,7 +61,7 @@ function install_sphinx_plugin() {
 }
 
 function install_usdt() {
-  ContractID=`sphinx-plugin usdt -addr 127.0.0.1 -port 8545 | grep "Contract:" | awk '{print $4}'`
+  ContractID=`sphinx-plugin usdt-erc20 -addr 127.0.0.1 -port 8545 | grep "Contract:" | awk '{print $4}'`
   info "ContractID = $ContractID" >> $LOG_FILE
   sed -i 's/contract_id.*/contract_id: "'$ContractID'"/g' /etc/SphinxPlugin/SphinxPlugin.viper.yaml
   sed -i 's/sphinx_proxy_addr.*/sphinx_proxy_addr: "'$SPHINX_PROXY_ADDR'"/g' /etc/SphinxPlugin/SphinxPlugin.viper.yaml
